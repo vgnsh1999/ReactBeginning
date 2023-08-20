d54b620eddb3504c202b9783a2def279836fbdfd
vgnsh1999 <vigneshvarathan@gmail.com>
2023-08-20 11:23:01 +0530

    passing data through props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,10 +1,14 @@
-function ExpenseItem() {
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
   return (
-    <div>
-      <h2>Expense Items!</h2>
-      <h2>Food Rs.10</h2>
-      <h2>Petrol Rs.100</h2>
-      <h2>Movies Rs.200</h2>
+    <div className="expense-item">
+      <div>{props.date.toDateString()}</div>
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <h2>{props.location}</h2>
+        <div className="expense-item__price">Rs.{props.amount}</div>
+      </div>
     </div>
   );
 }
