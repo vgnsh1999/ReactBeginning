a192cdd7105904b1c70f5ddd30ac907ad72e3444
vgnsh1999 <vigneshvarathan@gmail.com>
2023-08-20 16:09:06 +0530

    Making Components Smaller and Readable

--- src/App.js
+++ src/App.js
@@ -39,25 +39,25 @@ function App() {
         amount={expenses[0].amount}
         date={expenses[0].date}
         location={expenses[0].location}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[1].title}
         amount={expenses[1].amount}
         date={expenses[1].date}
         location={expenses[1].location}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[2].title}
         amount={expenses[2].amount}
         date={expenses[2].date}
         location={expenses[2].location}
-      ></ExpenseItem>
+      />
       <ExpenseItem
         title={expenses[3].title}
         amount={expenses[3].amount}
         date={expenses[3].date}
         location={expenses[3].location}
-      ></ExpenseItem>
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,11 @@
-import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import ExpenseDetail from "./ExpenseDetail";
 
 function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>{props.date.toDateString()}</div>
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <h2>{props.location}</h2>
-        <div className="expense-item__price">Rs.{props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
+      <ExpenseDetail title={props.title} location={props.location} amount={props.amount}  />
     </div>
   );
 }
