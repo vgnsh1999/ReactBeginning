fefac02e8c189db5ba669c02e8fe66a2b3f4f1ee
vgnsh1999 <vigneshvarathan@gmail.com>
2023-08-20 10:13:39 +0530

    Getting started to code in React.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div>
+      <h2>Lets get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense Items!</h2>
+      <h2>Food Rs.10</h2>
+      <h2>Petrol Rs.100</h2>
+      <h2>Movies Rs.200</h2>
+    </div>
+  );
+}
+
+export default ExpenseItem;
